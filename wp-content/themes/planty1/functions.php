41a3c04fca629c7ed59bddf3331e26f06f5b67df
Aurore <aurore.esslinger@gmail.com>
2023-05-09 15:09:19 +0200

    3 commit

--- wp-content/themes/planty1/functions.php
+++ wp-content/themes/planty1/functions.php
@@ -1,5 +1,5 @@
 <?php
-add_action('wp_enqueue_scripts', 'wpchild_enqueue_styles');
+add_action('wp_enqueue_scripts','theme_enqueue_styles' );
 function theme_enqueue_styles()
 {
     wp_enqueue_style('wpm-riverbank-style',get_template_directory_uri() . '/style.css');
